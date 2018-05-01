f998debf75f48fa7f8bef3e85c09b2b12691a257
Steve Torrens <stevetorrens@Steves-MacBook-Air.local>
2018-05-01 17:58:47 +1200

    Fetch different posts

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,10 +2,40 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
-const LoadSubreddit = ({dispatch}) => (
-  <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-    Fetch Posts
-  </button>
-)
+class LoadSubreddit extends React.Component {
+
+  constructor(props) {
+    super(props),
+    this.state = {
+      subReddit: ''
+    }
+
+    this.handleInput = this.handleInput.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleInput(e) {
+   console.log(e.target.value)
+   this.setState({
+     [e.target.name] : e.target.value
+   })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    this.props.dispatch(fetchPosts(this.state.subReddit))
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input onChange={this.handleInput} name='subReddit' placeholder="Redit post"></input>  
+        <button >
+          Fetch Posts
+        </button>
+      </form>
+  )
+  }
+}
 
 export default connect()(LoadSubreddit)
